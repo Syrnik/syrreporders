fcc921b24e739b3c4279350cafd37a997c575a74
Serge Rodovnichenko <sergerod@gmail.com>
2014-06-21 21:05:50 +0400

    Weekdays statistics done

--- lib/actions/shopSyrrepordersPluginBackendReport.action.php
+++ lib/actions/shopSyrrepordersPluginBackendReport.action.php
@@ -3,7 +3,7 @@
  * @author Serge Rodovnichenko <sergerod@gmail.com>
  *
  * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 /**
@@ -20,40 +20,16 @@ class shopSyrrepordersPluginBackendReportAction extends waViewAction
         $default_currency = wa()->getConfig()->getCurrency();
         $max_sales = 0;
         $max_orders = 0;
-        
+
         $sales = $Order->getOrderStats($timeframe);
-        
-        $stats['dow']['data'] = $Order->getOrderStatsByDow($timeframe);
-        $stats['dow']['max_count'] = 0;
-        $stats['dow']['max_sum'] = 0;
-        $stats['dow']['top']['count'] = array();
-        $stats['dow']['top']['sum'] = array();
-        
-        foreach($stats['dow']['data'] as $dow) {
-            
-            if((float)$dow['count'] > $stats['dow']['max_count']) {
-                $stats['dow']['max_count'] = (float)$dow['count'];
-                $stats['dow']['top']['count'] = array($dow);
-            } else if((float)$dow['count'] == $stats['dow']['max_count']) {
-                $stats['dow']['top']['count'][] = $dow;
-            }
-            
-            if((float)$dow['total'] > $stats['dow']['max_sum']) {
-                $stats['dow']['max_sum'] = (float)$dow['total'];
-                $stats['dow']['top']['sum'] = array($dow);
-            } else if((float)$dow['total'] == $stats['dow']['max_sum']) {
-                $stats['dow']['top']['sum'][] = $dow;
-            }
-            
-        }
-        
+
         foreach($sales as $row) {
             $max_sales = max($max_sales, (float)$row['total']);
             $max_orders = max($max_orders, (float)$row['count']);
         }
-        
+
         $chart_data = array();
-//        var_dump($sales);
+
         foreach($sales as $k=>$row) {
             $sales[$k]['total_percent'] = $max_sales ? ($row['total']*100 / ifempty($max_sales, 1)) : 0;
             $sales_data[] = array($row['date'], (float)$row['total']);
@@ -62,7 +38,7 @@ class shopSyrrepordersPluginBackendReportAction extends waViewAction
             $discount_data[] = array($row['date'], (float)$row['discount']);
             $tax_data[] = array($row['date'], (float)$row['tax']);
         }
-        
+
         $this->view->assign('chart_data', array(
             'sales'=>$sales_data,
             'count'=>$count_data,
@@ -73,6 +49,5 @@ class shopSyrrepordersPluginBackendReportAction extends waViewAction
         $this->view->assign('currency', $default_currency);
         $this->view->assign('table_data', $sales);
         $this->view->assign('group_by', $timeframe['group']);
-        $this->view->assign('stats', $stats);
     }
 }
