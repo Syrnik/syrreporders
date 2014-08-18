d49fc8f7061030c20f59b019c81fe91f631d8459
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-18 18:44:18 +0400

    Сохранение предпочтений графика

---
+++ lib/actions/shopSyrrepordersPluginBackendSetreppref.controller.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <sergerod@gmail.com>
+ * 
+ * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
+ * @version 1.0.0
+ */
+
+/**
+ * Report Preferences save
+ *
+ * @package webasyst.shop.plugin.syrreporders.controller
+ */
+class shopSyrrepordersPluginBackendSetrepprefController extends waJsonController
+{
+    /** @var waAppSettingsModel */
+    protected $Setting;
+    
+    public function execute()
+    {
+        $this->Setting = new waAppSettingsModel();
+
+        try {
+            $report = waRequest::post('report');
+            if(empty($report) || !in_array($report, array('orders', 'weekdays'))) {
+                throw new waException("Report ID is invalid or absent");
+            }
+            
+            $graph_settings = waRequest::post('orders_graph');
+            
+            if($graph_settings) {
+                $this->Setting->set(array('shop', 'syrreporders'), 'orders_graph', serialize($graph_settings));
+            }
+            
+        } catch (waException $ex) {
+            $this->setError($ex->getMessage());
+        }
+        
+        /** @todo Remove after pull request #58 (webasyst/webasyst-framework) merge */
+        $this->getResponse()->addHeader('Content-type', 'application/json');        
+    }
+    
+    protected function preExecute()
+    {
+        $this->Setting = new waAppSettingsModel();
+        parent::preExecute();
+    }
+}
