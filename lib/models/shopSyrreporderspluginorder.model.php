7a460016dc7a7a7509120c655e9bf37f83a73596
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-19 00:53:58 +0400

    Исправлены дни недели

--- lib/models/shopSyrreporderspluginorder.model.php
+++ lib/models/shopSyrreporderspluginorder.model.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @author Serge Rodovnichenko <sergerod@gmail.com>
- * 
+ *
  * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
  * @version 2.0.0
- * 
+ *
  * 2.0.0 - getOrderStatsByDow refactored
  */
 
@@ -18,19 +18,19 @@ class shopSyrreporderspluginorderModel extends shopOrderModel
     /**
      * Почти полная копия getSales(), с тем отличием, что запрос по дате создания,
      * а не по дате оплаты
-     * 
+     *
      * @param array $conditions
      * @return array
      */
     public function getOrderStats(array $conditions=array())
     {
         $defaults = array('start_date'=>NULL, 'end_date'=>NULL, 'group'=>'days');
-        
+
         $conditions = array_merge($defaults, $conditions);
-        
+
         $date_col = ($conditions['group'] == 'months') ? "DATE_FORMAT(o.create_datetime, '%Y-%m-01')" : 'DATE_FORMAT(o.create_datetime, "%Y-%m-%d")';
         $create_date_sql = self::getDateSql('o.create_datetime', $conditions['start_date'], $conditions['end_date']);
-        
+
         $sql = "SELECT
                     {$date_col} AS `date`,
                     SUM(o.total*o.rate) AS total,
@@ -62,7 +62,7 @@ class shopSyrreporderspluginorderModel extends shopOrderModel
         }
 
         $end_ts = strtotime(ifempty($conditions['end_date'], date('Y-m-d')));
-        
+
         for ($t = $start_ts; $t <= $end_ts; $t += 3600*24) {
             $date = date(($conditions['group'] == 'months') ? 'Y-m-01' : 'Y-m-d', $t);
             if (empty($result[$date])) {
@@ -81,48 +81,47 @@ class shopSyrreporderspluginorderModel extends shopOrderModel
 
         return $result;
     }
-    
+
     public function getOrderStatsByDow(array $conditions = array())
     {
-        $defaults = array('start_date'=>NULL, 'end_date'=>NULL, 'group'=>'days');        
+        $defaults = array('start_date'=>NULL, 'end_date'=>NULL, 'group'=>'days');
         $conditions = array_merge($defaults, $conditions);
-        $create_date_sql = self::getDateSql('o.create_datetime', $conditions['start_date'], $conditions['end_date']);
-        
+        $create_date_sql = self::getDateSql('DATE(o.create_datetime)', $conditions['start_date'], $conditions['end_date']);
 
-        $sql = "SELECT `dow`, AVG(`order_count`) AS `count`, AVG(`order_total`) AS `total` "
+        $sql = "SELECT `dow`, AVG(`order_count`) AS `count`, sum(`order_total`)/sum(`order_count`) AS `total` "
                 . "FROM ( "
                 . "SELECT WEEKDAY(create_datetime) AS dow,"
                 . "COUNT(*) as `order_count`,"
                 . "SUM(total*rate) AS `order_total` "
                 . "FROM `{$this->table}` o "
                 . "WHERE $create_date_sql "
-                . "GROUP BY create_datetime"
+                . "GROUP BY DATE(create_datetime)"
                 . ") AS temp "
                 . "GROUP BY `dow` "
                 . "ORDER BY `dow`";
 
         $result = $this->query($sql)->fetchAll('dow');
-        
+
         for($i=0;$i<7;$i++) {
             if(!isset($result[$i])) {
                 $result[$i] = array('dow'=>"$i", 'total'=>"0", 'count'=>"0");
             }
         }
-        
+
         ksort($result);
-        
+
         return $result;
     }
-    
+
     private function countDays($day, $start, $end)
-    {        
-        
+    {
+
         //get the day of the week for start and end dates (0-6)
         $w = array(date('w', $start), date('w', $end));
 
         //get partial week day count
         if ($w[0] < $w[1])
-        {            
+        {
             $partialWeekCount = ($day >= $w[0] && $day <= $w[1]);
         }else if ($w[0] == $w[1])
         {
