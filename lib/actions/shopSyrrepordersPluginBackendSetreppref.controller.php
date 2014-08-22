ad645fdb80eb737288ff9cf6fd6393938c98e403
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-22 22:51:27 +0400

    Saving order states preference

--- lib/actions/shopSyrrepordersPluginBackendSetreppref.controller.php
+++ lib/actions/shopSyrrepordersPluginBackendSetreppref.controller.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author Serge Rodovnichenko <sergerod@gmail.com>
- * 
+ *
  * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
  * @version 1.0.0
  */
@@ -15,7 +15,7 @@ class shopSyrrepordersPluginBackendSetrepprefController extends waJsonController
 {
     /** @var waAppSettingsModel */
     protected $Setting;
-    
+
     public function execute()
     {
         $this->Setting = new waAppSettingsModel();
@@ -25,21 +25,26 @@ class shopSyrrepordersPluginBackendSetrepprefController extends waJsonController
             if(empty($report) || !in_array($report, array('orders', 'weekdays'))) {
                 throw new waException("Report ID is invalid or absent");
             }
-            
+
             $graph_settings = waRequest::post('orders_graph');
-            
+            $orders_states = waRequest::post('orders_state');
+
             if($graph_settings) {
                 $this->Setting->set(array('shop', 'syrreporders'), 'orders_graph', serialize($graph_settings));
             }
-            
+
+            if($orders_states) {
+                $this->Setting->set(array('shop', 'syrreporders'), 'orders_states', serialize($orders_states));
+            }
+
         } catch (waException $ex) {
             $this->setError($ex->getMessage());
         }
-        
+
         /** @todo Remove after pull request #58 (webasyst/webasyst-framework) merge */
-        $this->getResponse()->addHeader('Content-type', 'application/json');        
+        $this->getResponse()->addHeader('Content-type', 'application/json');
     }
-    
+
     protected function preExecute()
     {
         $this->Setting = new waAppSettingsModel();
