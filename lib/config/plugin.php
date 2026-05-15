8bc0f0ff05a05749147b3c29876d07c82982afc0
Serge Rodovnichenko <sergerod@gmail.com>
2026-05-15 12:47:35 +0300

    Рефакторинг: упрощение добавления ресурсов и форматирование кода
    - Заменены вызовы `$response->addCss/addJs()` на методы плагина `$this->addCss/addJs()`
    - Упрощены пути к ресурсам (удалены полные пути, используются относительные)
    - Удалены версии из URL JS файлов
    - Добавлена типизация возвращаемого значения метода `backendReports(): array`
    - Исправлено форматирование: пробелы вокруг операторов, удалены пробелы в конце строк
    - Обновлены сведения об авторских правах в заголовке файла
    - Замена `array()` на короткий синтаксис `[]`

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,17 +1,17 @@
 <?php
 
-return array(
+return [
     'name' => 'Additional Reports',
     'description' => 'More reports',
-    'vendor'=>670917,
-    'version'=>'2.2.2',
-    'shop_settings' => TRUE,
-    'frontend'    => FALSE,
-    'icons'=>array(
+    'vendor' => 670917,
+    'version' => '2.2.2',
+    'shop_settings' => true,
+    'frontend' => false,
+    'icons' => [
         16 => 'img/actions-office-chart-line-percentage-icon.png'
-        ),
-    'handlers' => array(
+    ],
+    'handlers' => [
         'backend_reports' => 'backendReports',
-    ),
-    'locale' => array('en_US', 'ru_RU')
-);
+    ],
+    'locale' => ['en_US', 'ru_RU']
+];
--- lib/shopSyrreporders.plugin.php
+++ lib/shopSyrreporders.plugin.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @author Serge Rodovnichenko <sergerod@gmail.com>
- * 
+ *
  * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
- * @version 2.2.1
+ * @copyright Serge Rodovnichenko <serge@syrnik.com>, 2014-2026
  */
 
 /**
@@ -13,26 +13,26 @@
  */
 class shopSyrrepordersPlugin extends shopPlugin
 {
-    const PLUGIN_ID='syrreporders';
+    const PLUGIN_ID = 'syrreporders';
 
     /**
      * Handler for backend_reports hook
-     * 
+     *
      * @return array
+     * @throws SmartyException
+     * @throws waException
      */
-    public function backendReports()
+    public function backendReports(): array
     {
         $view = wa()->getView();
         $settings = $this->getSettings();
         $view->assign('settings', $settings);
         $content = $view->fetch($this->path . "/templates/menuitem.html");
-        $response = wa()->getResponse();
-        $response->addCss('wa-apps/shop/plugins/syrreporders/css/syrreporders.css');
-        $response->addJs('wa-apps/shop/plugins/syrreporders/js/chartjs/chart.umd.min.js');
-        $response->addJs('wa-apps/shop/plugins/syrreporders/js/chartjs/chartjs-adapter-date-fns.bundle.min.js');
-        $response->addJs('wa-apps/shop/plugins/syrreporders/js/syrreporders-orders.js?v=2.4.0');
-        $response->addJs('wa-apps/shop/plugins/syrreporders/js/syrreporders-weekdays.js?v=2.4.0');
-        return array('menu_li' => $content);
+        $this->addCss('css/syrreporders.css');
+        $this->addJs('js/chartjs/chart.umd.min.js');
+        $this->addJs('js/chartjs/chartjs-adapter-date-fns.bundle.min.js');
+        $this->addJs('js/syrreporders-orders.js');
+        $this->addJs('js/syrreporders-weekdays.js');
+        return ['menu_li' => $content];
     }
-
 }
