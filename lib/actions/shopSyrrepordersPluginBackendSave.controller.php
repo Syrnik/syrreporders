dbf3595b88fcb9002c37a1012eddb737f797596d
Serge Rodovnichenko <sergerod@gmail.com>
2026-05-16 02:01:23 +0300

    Рефакторинг сохранения настроек: перенос логики в плагин и обновление UI 2.0
    - Переписан контроллер SaveController с использованием preExecute() и типизацией
    - Перенесена логика сохранения в метод plugin.saveSettings()
    - Добавлен новый шаблон settings/Settings.html с UI 2.0 (список, иконки FontAwesome)
    - Обновлен legacy-шаблон скрытыми полями для unchecked чекбоксов
    - Добавлен конфиг settings.php с дефолтными значениями
    - Исправлены отступы и пустые строки в PHP-файле

--- lib/actions/shopSyrrepordersPluginBackendSave.controller.php
+++ lib/actions/shopSyrrepordersPluginBackendSave.controller.php
@@ -1,29 +1,36 @@
 <?php
 /**
  * @author Serge Rodovnichenko <sergerod@gmail.com>
- * 
+ *
  * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
- * @version 1.0.0
  */
 
 /**
  * Settings Save Action
  *
  * @package webasyst.shop.plugin.syrreporders.controller
- * @deprecated since version 1.2.0
  */
 class shopSyrrepordersPluginBackendSaveController extends waJsonController
 {
-    public function execute()
+    protected shopSyrrepordersPlugin $plugin;
+
+    protected function preExecute()
     {
-        $Setting = new waAppSettingsModel();
-        $data = waRequest::post("shop_syrreporders");
-        
-        $data = array_merge(array('report_orders'=>0, 'report_weekdays'=>0), $data);
-        
-        $Setting->set(array('shop', 'syrreporders'), 'report_orders', $data['report_orders']);
-        $Setting->set(array('shop', 'syrreporders'), 'report_weekdays', $data['report_weekdays']);
-        
+        parent::preExecute();
+
+        $this->plugin = wa('shop')->getPlugin('syrreporders');
+    }
+
+    /**
+     * @return void
+     * @throws waException
+     */
+    public function execute(): void
+    {
+        $settings = waRequest::post("shop_syrreporders");
+
+        $this->plugin->saveSettings($settings);
+
         $this->response['message'] = _wp("Saved");
     }
 }
---
+++ lib/config/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'report_orders' => [
+        'value' => '1',
+    ],
+    'report_weekdays' => [
+        'value' => '1',
+    ]
+];
